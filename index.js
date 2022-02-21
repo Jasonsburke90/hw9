bd0eb63198c39c7f2176a2f6781ea87b41db8b6b
unknown <jasonsburke90@gmail.com>
2022-02-20 21:33:08 -0600

    more tests added

--- index.js
+++ index.js
@@ -5,37 +5,45 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "description",
-      message: "Describe your project",
-    },
-    {
-      type: "input",
-      name: "installation",
-      message: "Include any steps required for installation here",
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "Provide instructions and examples for use ",
-    },
-    {
-      type: "input",
-      name: "contribution",
-      message:
-        "Provide instructions for having other developers contribute to your project here",
-    },
-    {
-      type: "input",
-      name: "tests",
-      message:
-        "Provide descriptions and examples of how to run your tests here",
-    },
-  ]);
-};
+const questions = [
+  {
+    type: "input",
+    name: "description",
+    message: "Describe your project",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Include any steps required for installation here",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide instructions and examples for use ",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message:
+      "Provide instructions for having other developers contribute to your project here",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Provide descriptions and examples of how to run your tests here",
+  },
+  {
+    type: "input",
+    name: "githubName",
+    message: "What is your github username?",
+  },
+  {
+    type: "input",
+    name: "emailAddress",
+    message:
+      "What is your email address in case anyone wishes to contact you with questions about your project?",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
